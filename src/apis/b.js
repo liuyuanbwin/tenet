234a449dd71d7e8dcf80029c58ed174f8b75404a
刘远 <liuy@necibook.com>
2020-10-30 00:09:12 +0800

    [t0]联表查询

--- src/apis/b.js
+++ src/apis/b.js
@@ -1,6 +1,16 @@
-import {_b, _c, _Ib, _lks} from '@/utils'
+import { _b, _c, _Ib, _lks } from '@/utils'
 
 export const allBs = async function () {
   let bs = await _b.where({}).get()
-  return bs
+  let _bs = []
+  bs.data.forEach(async (_b) => {
+    let lks = await _c
+      .where({
+        _id: _b.cId
+      })
+      .get()
+    _bs.push({ ...lks.data[0], ..._b })
+    console.log(_bs)
+  })
+  return _bs
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,59 @@
-import React from 'react'
-import { auth } from '@/utils'
+import React, { useEffect } from 'react'
+import { auth, db } from '@/utils'
 import { useHistory } from 'react-router'
 import Table from '@/components/Table'
-import {allBs} from '@/apis/b'
+import { allBs } from '@/apis/b'
 
 export const Home: React.FC<{}> = () => {
   let state = auth.hasLoginState()
   let history = useHistory()
-  console.log('%cHome.tsx line:10 state', 'color: #26bfa5;', state);
+  console.log('%cHome.tsx line:10 state', 'color: #26bfa5;', state)
   if (!state) {
     history.push('/login')
   }
-  allBs().then(res => console.log('%cHome.tsx line:14 res', 'color: #26bfa5;', res))
+
+  useEffect(() => {
+    const fetchBill = async () => {
+      let result = await allBs()
+      console.log(result)
+    }
+    fetchBill()
+  }, [])
+
   const dataSource = [
     {
       key: '1',
       name: '胡彦斌',
       age: 32,
-      address: '西湖区湖底公园1号',
+      address: '西湖区湖底公园1号'
     },
     {
       key: '2',
       name: '胡彦祖',
       age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
+      address: '西湖区湖底公园1号'
+    }
+  ]
   const columns = [
     {
       title: '姓名',
       dataIndex: 'name',
-      key: 'name',
+      key: 'name'
     },
     {
       title: '年龄',
       dataIndex: 'age',
-      key: 'age',
+      key: 'age'
     },
     {
       title: '住址',
       dataIndex: 'address',
-      key: 'address',
-    },
-  ];
+      key: 'address'
+    }
+  ]
   return (
-  <div>
-    <Table
-      columns={columns}
-      dataSource={dataSource}
-    >
-    </Table>
-  </div>
+    <div>
+      <Table columns={columns} dataSource={dataSource} />
+    </div>
   )
 }
